f65a390400274f495fa4e1f9c916c13833d1924b
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-08-16 20:30:41 -0400

    Working on Robot Gladiators

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,8 +11,26 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
-  window.alert("Welcome to Robot Gladiators!");
+ // Alert users that they are starting the round
+ window.alert("Welcome to Robot Gladiators!");
+
+ //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+ enemyHealth-=playerAttack;
+
+ // Log a resulting message to the console so we know that it worked.
+ console.log(enemyName+ " Health => "+enemyHealth);
+
+ // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+ playerHealth-=enemyAttack;
+
+ // Log a resulting message to the console so we know that it worked.
+ console.log(playerName+ " Health => "+playerHealth);
 };
+
+//Anothe way
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
 
 /********************************************************************/
 
@@ -24,7 +42,9 @@ var fight = function() {
 /**************************Execution ********************************/
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+console.log("Robot Name: "+playerName, 
+            "// Points of Attack: "+playerAttack, 
+            "// Points of health: "+playerHealth);
 
 fight();
 
