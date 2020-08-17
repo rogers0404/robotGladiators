56045cce38a1103c60cb39d8b966fc47c56dbba3
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-08-16 20:37:33 -0400

    Working on Variables amd Function. Robot Gladiators

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,12 +25,30 @@ var fight = function() {
 
  // Log a resulting message to the console so we know that it worked.
  console.log(playerName+ " Health => "+playerHealth);
-};
+
 
 //Anothe way
 console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
+  
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+  // check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+
+};
 
 /********************************************************************/
 
@@ -47,6 +65,7 @@ console.log("Robot Name: "+playerName,
             "// Points of health: "+playerHealth);
 
 fight();
+
 
 
 
