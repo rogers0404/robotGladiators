91d013711f40a8d359866a741fc4c942b8d45e67
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-08-16 20:14:42 -0400

    Starting Robot Gladiator in Javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,37 @@
 /********************   Robot Gladiators ****************************/
 
-window.alert("This is an alert! JavaScript is running!");
+/*******************   Variables here   *****************************/
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+/********************************************************************/
+
+/*******************   Functions here   *****************************/
+
+
+/********************************************************************/
+
+/**************************Execution ********************************/
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+fight();
+
+
+
+
+
+
+
+/********************************************************************/
